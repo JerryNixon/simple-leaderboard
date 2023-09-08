809266b1f3c5b0a309a77f1d357d41bf88706c2f
Jerry Nixon <1749983+JerryNixon@users.noreply.github.com>
2023-09-07 20:05:12 -0600

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,26 +1,22 @@
 CREATE TABLE [dbo].[game](
-	[id] [int] NOT NULL,
-	[name] [varchar](500) NOT NULL,
-	CONSTRAINT [PK_game] PRIMARY KEY CLUSTERED ([id] ASC)
+	[id] [int] PRIMARY KEY,
+	[name] [varchar](500) NOT NULL
 );
 GO
 
 CREATE TABLE [dbo].[gamer](
-	[id] [int] NOT NULL,
-	[name] [varchar](500) NOT NULL,
-	[email] [varchar](500) NOT NULL,
-	CONSTRAINT [PK_gamer] PRIMARY KEY CLUSTERED ([id] ASC)
+	[email] [varchar](500) PRIMARY KEY,
+	[name] [varchar](500) NOT NULL
 );
 GO
 
 CREATE TABLE [dbo].[score](
-	[id] [int] NOT NULL,
-	[gamer_id] [int] NOT NULL,
+	[id] [int] PRIMARY KEY,
+	[gamer_email] [varchar](500) NOT NULL,
 	[game_id] [int] NOT NULL,
 	[date] [datetime] NOT NULL DEFAULT (getdate()),
 	[score] [bigint] NOT NULL DEFAULT (0),
-	CONSTRAINT [PK_score] PRIMARY KEY CLUSTERED ([id] ASC),
 	CONSTRAINT [FK_score_game] FOREIGN KEY([game_id]) REFERENCES [dbo].[game] ([id]),
-	CONSTRAINT [FK_score_gamer] FOREIGN KEY([gamer_id]) REFERENCES [dbo].[gamer] ([id])
+	CONSTRAINT [FK_score_gamer] FOREIGN KEY([gamer_email]) REFERENCES [dbo].[gamer] ([email])
 );
 GO
